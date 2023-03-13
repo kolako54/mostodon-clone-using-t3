efe248a02d5a171b0bb0021fca753998bcd9acc3
Elio <parhamwolf051@gmail.com>
2023-03-13 06:46:10 +0330

    Add dayjs for date formatting, images for Gravatar, and infinite scrolling for timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,13 +1,78 @@
 import { api } from "../utils/api";
+import type { RouterOutputs } from "../utils/api";
 import CreateTweet from "./CreateTweet";
+import Image from "next/image";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocal from "dayjs/plugin/updateLocale";
+dayjs.extend(relativeTime);
+dayjs.extend(updateLocal);
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s ago",
+    s: "1m",
+    m: "1m",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh",
+    d: "1d",
+    dd: "%dh",
+    M: "1M",
+    MM: "%dM",
+    y: "1y",
+    yy: "%dy",
+  },
+});
+const Tweet = ({
+  tweet,
+}: {
+  tweet: RouterOutputs["tweet"]["timeline"]["tweets"][number];
+}) => {
+  return (
+    <div className="mb-4 border-b-2 border-gray-500">
+      <div className="flex p-2">
+        {tweet.author.image && (
+          <Image
+            src={tweet.author.image}
+            alt={`${tweet.author.name} profile picture`}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        )}
+        <div className="ml-2">
+          <div className="align-center flex">
+            <p className="font-bold">{tweet.author.name}</p>
+            <p className="text-sm text-gray-400">
+              - {dayjs(tweet.createdAt).fromNow()}
+            </p>
+          </div>
+          <div>{tweet.text}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Timeline = () => {
-  const tweets = api.tweet.timeline.useQuery({
-    limit: 2,
+  const {data, hasNextPage, fetchNextPage, isFetching} = api.tweet.timeline.useInfiniteQuery({
+    limit: 10,
+  },
+  {
+    getNextPageParam: (lastPage) => lastPage.nextCursor
   });
+  const tweets = data?.pages.flatMap((page) => page.tweets) ?? []
+  console.log("dataaa", data);
   return (
     <div>
       <CreateTweet />
-      {JSON.stringify(tweets)}
+      <div className="border-l-2 border-r-2 border-t-2 border-gray-500">
+        {tweets.map((tweet) => {
+          return <Tweet key={tweet.id} tweet={tweet} />;
+        })}
+        <button onClick={() => void fetchNextPage()} disabled={!hasNextPage || isFetching}>load next</button>
+      </div>
     </div>
   );
 };
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -42,9 +42,25 @@ export const tweetRouter = createTRPCRouter({
             createdAt: "desc",
           },
         ],
+        cursor: cursor ? {id: cursor} : undefined,
+        include: {
+          author: {
+            select: {
+              name: true,
+              image: true,
+              id: true,
+            },
+          },
+        },
       });
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (tweets.length > limit) {
+        const nextItem = tweets.pop() as (typeof tweets)[number];
+        nextCursor = nextItem.id;
+      }
       return {
         tweets,
+        nextCursor,
       };
     }),
 });
